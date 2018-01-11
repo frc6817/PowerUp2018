24c2dcfac0b1fc3643ff6ca098b369e0281f27a3
Matthew Fan <fn.matthew@outlook.com>
2018-01-10 19:06:57 -0600

    Auto Line auto shell

---
+++ src/main/java/frc/team6817/robot/Autonomous/AutoLine.java
@@ -0,0 +1,21 @@
+package frc.team6817.robot.Autonomous;
+
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+
+/**
+ * Auto Line autonomous- crosses the auto line for 5 points
+ *
+ * A game changer
+ */
+public class AutoLine extends CommandGroup
+{
+    /**
+     * Lays out the commands for the AutoLine autonomous
+     */
+    public AutoLine()
+    {
+
+    }
+}
