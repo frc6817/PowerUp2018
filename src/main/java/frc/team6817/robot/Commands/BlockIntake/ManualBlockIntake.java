537d6cd0787564dbdc7d2b323f9a2ababb8bda5e
Matthew Fan <fn.matthew@outlook.com>
2018-03-11 13:07:05 -0500

    Nonblocking block intake code
    Signed-off-by: FusionForScience <fn.matthew@outlook.com>

--- src/main/java/frc/team6817/robot/Commands/BlockIntake/ManualBlockIntake.java
+++ src/main/java/frc/team6817/robot/Commands/BlockIntake/ManualBlockIntake.java
@@ -1,13 +1,13 @@
 package frc.team6817.robot.Commands.BlockIntake;
 
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.team6817.robot.OI;
 
 import static frc.team6817.robot.Robot.blockIntake;
-import static frc.team6817.robot.RobotMap.leftIntakeController;
-import static frc.team6817.robot.RobotMap.rightIntakeController;
+import static frc.team6817.robot.RobotMap.*;
 
 
 public class ManualBlockIntake extends Command
@@ -35,11 +35,11 @@ public class ManualBlockIntake extends Command
 
         if(OI.controller2().getBumperPressed(GenericHID.Hand.kLeft))
         {
-            new ActuateArms(false).execute();
+            dualIntake.set(DoubleSolenoid.Value.kForward);
         }
         else if(OI.controller2().getBumperPressed(GenericHID.Hand.kRight))
         {
-            new ActuateArms(true).execute();
+            dualIntake.set(DoubleSolenoid.Value.kReverse);
         }
 
         leftIntakeController.set(intakePower * _MULTIPLIER);
